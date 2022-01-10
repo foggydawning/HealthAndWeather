929e69dffdca8df295b5bc727984b4d0d74699d0
foggydawning <foggydawning@gmail.com>
2022-01-10 14:58:25 +0300

    Создана страница с регистрацией пользователя без подключения базы данных

--- website/app/routes.py
+++ website/app/routes.py
@@ -1,8 +1,17 @@
-from flask import render_template
+from flask import render_template, flash
+from werkzeug.utils import redirect
 from app import app
+from app.forms import RegistrationForm
 
-@app.route('/')
+
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    return render_template("index.html")
+
+
 @app.route('/registration', methods=['GET', 'POST'])
 def registration():
-    return render_template('registration.html')
-
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        return redirect("/index")
+    return render_template('registration.html', title='Registration', form=form)
