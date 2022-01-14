21f0e75c79c152d38c45fa5c68bd9ba08ddc74e8
foggydawning <foggydawning@gmail.com>
2022-01-14 21:08:15 +0300

    degrade

--- website/app/forms.py
+++ website/app/forms.py
@@ -5,38 +5,21 @@ from wtforms.validators import DataRequired
 
 class RegistrationForm(FlaskForm):
     email = EmailField(
-<<<<<<< HEAD
-        'Username',
-        render_kw={"placeholder": "Введите адрес электронной почты"},
-=======
         'Username', 
         render_kw={"placeholder": "Введите адрес электронной почты"}, 
->>>>>>> main
         validators=[DataRequired()]
     )
 
     password = PasswordField(
-<<<<<<< HEAD
-        'Password',
-=======
         'Password', 
->>>>>>> main
         render_kw={"placeholder": "Введите пароль"},
         validators=[DataRequired()]
     )
 
     repeat_password = PasswordField(
-<<<<<<< HEAD
-        'Password',
-=======
         'Password', 
->>>>>>> main
         render_kw={"placeholder": "Повторите пароль"},
         validators=[DataRequired()]
     )
 
-<<<<<<< HEAD
     submit = SubmitField('Зарегистрироваться')
-=======
-    submit = SubmitField('Зарегистрироваться')
->>>>>>> main
