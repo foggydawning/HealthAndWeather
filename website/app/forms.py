4380909a9c4aaa4c8e6e89096971f9e64869225e
foggydawning <foggydawning@gmail.com>
2022-01-21 00:38:32 +0300

    виртуальное окружение

--- website/app/forms.py
+++ website/app/forms.py
@@ -21,8 +21,7 @@ class RegistrationForm(FlaskForm):
         render_kw={"placeholder": "Повторите пароль"},
         validators=[DataRequired(), EqualTo('password')]
     )
-
-
+    
     submit = SubmitField('Зарегестрироваться')
 
     def validate_username(self, username):
@@ -33,7 +32,8 @@ class RegistrationForm(FlaskForm):
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
-            raise ValidationError('Please use a different email address.')        
+            raise ValidationError('Please use a different email address.')
+
 class LoginForm(FlaskForm):
     email = EmailField(
         'Username', 
