703c9a21123361121e49f413f19d90a6204026c9
foggydawning <foggydawning@gmail.com>
2022-01-22 00:23:15 +0300

    fix errors

--- website/app/forms.py
+++ website/app/forms.py
@@ -24,11 +24,6 @@ class RegistrationForm(FlaskForm):
     
     submit = SubmitField('Зарегестрироваться')
 
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different username.')
-
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
