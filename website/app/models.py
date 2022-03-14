ab72d541871ec4c2c1b18f9ee3fb069811cedb2c
foggydawning <foggydawning@gmail.com>
2022-03-14 15:14:20 +0300

    add table data to db

--- website/app/models.py
+++ website/app/models.py
@@ -2,12 +2,12 @@ from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
-
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    data = db.relationship('Data', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -17,6 +17,15 @@ class User(UserMixin, db.Model):
 
     def check_password(self,password):
         return check_password_hash(self.password_hash, password)
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    well_being = db.Column(db.Integer)
+    is_head_hurts = db.Column(db.Integer)
+    is_high_pressure = db.Column(db.Integer)
+    temperature = db.Column(db.Integer)
+    humidity = db.Column(db.Integer)
 
 @login.user_loader
 def load_user(id):
