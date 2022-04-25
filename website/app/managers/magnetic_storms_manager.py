8a56169fd59cfea28f2d5a2b47d9bfc3460870cb
ILIA CHUB <foggydawning@gmail.com>
2022-04-25 06:04:25 +0300

    create MagneticStormsManager

--- website/app/managers/magnetic_storms_manager.py
+++ website/app/managers/magnetic_storms_manager.py
@@ -1,21 +1,31 @@
-# import os
-# import urllib.request
-# from datetime import datetime
-#
-# import nasapy
-#
-# # Initialize Nasa class by creating an object:
-# k = "Hdd633EbwbQ8elJKig6Ub8AJHhXS5MYKBev6yKdS"
-# nasa = nasapy.Nasa(key=k)
-# # Get today’s date in YYYY-MM-DD format:
-# d = datetime.today().strftime("%Y-%m-%d")
+import time
+from typing import Optional
 
-from random import randint
+from requests import get
 
 
 class MagneticStormsManager:
     def __init__(self):
         self.api_key = "Hdd633EbwbQ8elJKig6Ub8AJHhXS5MYKBev6yKdS"
 
-    def get_magnetic_storms(self) -> int:
-        return randint(3, 6)
+    def get_magnetic_storms(self) -> Optional[int]:
+        request_string = "https://api.nasa.gov/DONKI/GST?"
+        try:
+            startDate = time.strftime("%Y-%m-%d")
+            res = get(
+                request_string,
+                params={
+                    "startDate": startDate,
+                    "location": "Earth",
+                    "catalog": "SWRC_CATALOG",
+                    "api_key": self.api_key,
+                },
+            )
+            if not res.content:
+                return 0
+            data = res.json()
+            value = int(data[-1]["allKpIndex"][-1]["kpIndex"])
+            return value
+        except Exception as e:
+            print(f"Ошибка при получении магнитных бурь {e}")
+            return None
--- website/app/managers/weather_manager.py
+++ website/app/managers/weather_manager.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from app.managers.magnetic_storms_manager import MagneticStormsManager
 from app.managers.openweather_manager import OpenweatherManager
 from app.models import Weather
@@ -10,11 +12,19 @@ class WeatherManager:
         self.openweather_manager = OpenweatherManager(lat, lon)
         self.magnetic_storms_managers = MagneticStormsManager()
 
-    def get_weather(self) -> Weather:
+    def get_weather(self) -> Optional[Weather]:
         temperature = self.openweather_manager.get_temperature()
         pressure = self.openweather_manager.get_pressure()
         magnetic_storms = self.magnetic_storms_managers.get_magnetic_storms()
-        weather = Weather(
-            pressure=pressure, temperature=temperature, magnetic_storms=magnetic_storms
-        )
-        return weather
+        if (
+            temperature is not None
+            and pressure is not None
+            and magnetic_storms is not None
+        ):
+            weather = Weather(
+                pressure=pressure,
+                temperature=temperature,
+                magnetic_storms=magnetic_storms,
+            )
+            return weather
+        return None
